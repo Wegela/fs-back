b6d0d086f5b8f00b686771c261629fa78c26684b
Wegela <gwenael.giroux@gmail.com>
2022-09-14 11:46:32 +0200

    change

--- src/main/java/fr/polytech/fsback/Service/BibliothequeService.java
+++ src/main/java/fr/polytech/fsback/Service/BibliothequeService.java
@@ -1,11 +1,33 @@
 package fr.polytech.fsback.Service;
 
+import fr.polytech.fsback.Controllers.Livre;
+import fr.polytech.fsback.dto.BibliothequeDto;
+import fr.polytech.fsback.dto.LivreDto;
+import fr.polytech.fsback.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class BibliothequeService {
 
-    public final List<>
+    private final List<BibliothequeDto> listeDeBibliotheque = new ArrayList<>();
+    private final LivreService livreService;
+    public BibliothequeDto getBibliothequeById(final String id){
+        return this.listeDeBibliotheque.stream().filter(bibliothequeDto -> bibliothequeDto.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException("bibliotheque with this is not exist"));
+    }
+
+    public LivreDto addLivreToBiblio(final String bibliothequeId, final String livreId){
+        final BibliothequeDto bibliotheque = this.getBibliothequeById(bibliothequeId);
+        final LivreDto livreToAdd = this.livreService.getLivreById(livreId);
+
+        bibliotheque.getLivres().add(livreToAdd);
+
+        return livreToAdd;
+    }
 }
---
+++ src/main/java/fr/polytech/fsback/dto/BibliothequeDto.java
@@ -0,0 +1,23 @@
+package fr.polytech.fsback.dto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.lang.invoke.DelegatingMethodHandle$Holder;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BibliothequeDto {
+
+    @JsonProperty("id")
+    public String id;
+    @JsonProperty("nom")
+    private String nom;
+    @JsonProperty("livres")
+    private List<LivreDto> livres;
+}
