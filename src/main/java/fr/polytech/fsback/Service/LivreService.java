e12aced987e0d0f79ad5c7cce8d27caf6f265b94
Wegela <gwenael.giroux@gmail.com>
2022-09-14 14:58:06 +0200

    change

--- src/main/java/fr/polytech/fsback/Service/LivreService.java
+++ src/main/java/fr/polytech/fsback/Service/LivreService.java
@@ -1,8 +1,10 @@
 package fr.polytech.fsback.Service;
 
 import fr.polytech.fsback.Controllers.Livre;
+import fr.polytech.fsback.Entity.LivreEntity;
 import fr.polytech.fsback.dto.LivreDto;
 import fr.polytech.fsback.exception.NotFoundException;
+import fr.polytech.fsback.repository.LivreRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,32 +15,14 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 @Service
+@RequiredArgsConstructor
 public class LivreService {
-    private Collection<LivreDto> livres = new ArrayList<LivreDto>(Arrays.asList(LivreDto.builder().ID("1").titre("abc").build(), LivreDto.builder().ID("2").titre("def").build()));
 
-    public LivreService() {
-        LivreDto createdLivre = LivreDto.builder().ID(String.valueOf(Math.random())).titre("Harry Potter").build();
-        livres.add(createdLivre);
-    }
+    private LivreRepository livreRepository;
 
-
-    public LivreDto getLivreById(String id) {
-        return this.livres.stream().filter(livreDto -> livreDto.getID()==id).findFirst()
+    public LivreEntity getLivreById(String id) {
+        return this.livreRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Livre with id " + id + " doesn't exist"));
     }
 
-    public  Collection<LivreDto> getLivres(){
-        return livres;
-    }
-
-    public LivreDto addLivres(String name){
-        LivreDto createdLivre = LivreDto.builder().ID(String.valueOf(Math.random())).titre(name).build();
-        livres.add(createdLivre);
-        return (createdLivre);
-    }
-
-    public void deleteLivres(String id){
-        livres.remove(getLivreById(id));
-
-    }
 }
